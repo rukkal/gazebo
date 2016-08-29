dd633f03dd4be4e3b873ac8c7a466ee97f2674ac
Kean Mariotti <kean.mariotti@cscs.ch>
2016-08-29 09:12:43 +0200

    added messages for user to start_gzweb.sh

--- scl7.2/start_gzweb.sh
+++ scl7.2/start_gzweb.sh
@@ -3,9 +3,16 @@
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPT_PATH/load-environment
 
+logfile_gzserver=/tmp/gzserver.log
+logfile_gzweb=/tmp/gzweb.log
+
 #nohup is used for a quick and dirty demonization here
 if pgrep gzserver &>/dev/null ; then
-   nohup $PREFIX/bin/gzserver --verbose 0<&- &>/tmp/gzserver.log &
+   echo "gzserver already running"
+else
+   echo "starting gzserver... logging to $logfile_gzserver"
+   nohup $PREFIX/bin/gzserver --verbose 0<&- &>$logfile_gzserver &
 fi
-nohup $HOME/osrf-gzweb-b027171f5aa1/start_gzweb.sh 0<&- &>/tmp/gzweb.log &
+echo "starting gzweb... logging to $logfile_gzweb"
+nohup $HOME/osrf-gzweb-b027171f5aa1/start_gzweb.sh 0<&- &>$logfile_gzweb &
 
